9c6b2701f70365f9940a10c7706fc98e512e61d4
Max Wagner <11011248@stud.hochschule-heidelberg.de>
2019-10-07 14:53:39 +0200

    Main menu logic changed

--- CarGo/Menus/MainMenu.cs
+++ CarGo/Menus/MainMenu.cs
@@ -13,47 +13,36 @@ namespace CarGo
 {
     public class MainMenu
     {
-        private List<RotRectangle> ButtonHitboxes; //Button as Hitbox for Selection, 
         private Texture2D MainMenuBackground;
         private SpriteBatch spriteBatch;
         private SpriteFont spriteFont;
-        private RotRectangle carrierBox;
-        //private RotRectangle carBox;
         private Texture2D carrierTexture;
-        //private Texture2D carTexture;
-        private Vector2[] carrierBoxConers;
-        //private Vector2[] carBoxCorners;
-        private Vector2[] buttonBoxCorners;
         private SoundEffectInstance soundHorn;
         private Game1 theGame;
-        private KeyboardState previousKeyBoardState;
         private GamePadState previousState;
+
+        private List<Vector2> buttons;
+        private int stage;
 
         public MainMenu(SpriteBatch spriteBatchInit,Vector2 screenSize, ContentManager contentManager, Game1 game)
         {
             spriteBatch = spriteBatchInit;
             theGame = game;
-            previousKeyBoardState = Keyboard.GetState();
+            stage = 0;
 
             //Boxes
             //Create Buttons 
-            ButtonHitboxes = new List<RotRectangle>(); 
-            for(int i = 0; i<4; i++)
+
+            buttons = new List<Vector2>();
+            for (int i = 0; i < 5; i++)
             {
-                ButtonHitboxes.Add(new RotRectangle(0, new Vector2(450, 360 + (int)i * 100), new Vector2(100, 32)));
+                buttons.Add(new Vector2(300, 300 + (int)i * 100));
             }
-            //Selectionboxes
-            carrierBox = new RotRectangle(0, new Vector2(300, 360), new Vector2(300, 55));
-            
-            //carBox = new RotRectangle(1.57f, new Vector2(160, 420), new Vector2(50, 50));
-            //carBoxCorners = carBox.Corners;
-            
+
             //Texture
             //Set Background 
-            //TextureCollection.Instance.loadTextures(contentManager);
             MainMenuBackground = TextureCollection.Instance.GetTexture(TextureType.MainMenuBackground);
             carrierTexture = TextureCollection.Instance.GetTexture(TextureType.MainMenuCarrier);
-            //carTexture = TextureCollection.Instance.GetTexture(TextureType.Car_Medium);
 
             //Set font for Buttontext 
             FontCollection.Instance.LoadFonts(contentManager);
@@ -68,43 +57,41 @@ namespace CarGo
             spriteBatch.Begin();
 
             //Draw Background and Selection
-            //spriteBatch.Draw(texture, hitbox.Center - offset, null, Color.White, hitbox.RotationRad, hitbox.Offset, 1.0f, SpriteEffects.None, 0f);
             spriteBatch.Draw(MainMenuBackground, new Vector2(0, 0), Color.White);
-            carrierBoxConers = carrierBox.Corners;
-            spriteBatch.Draw(carrierTexture, carrierBoxConers[2], Color.White);
-            //spriteBatch.Draw(carTexture, carBox.Center - carBox.Offset, null, Color.White, carBox.RotationRad, carBox.Offset, 1.0f, SpriteEffects.None, 0f);
+            //carrierBoxConers = carrierBox.Corners;
+            spriteBatch.Draw(carrierTexture, buttons[stage] - new Vector2(buttons[stage].X, 25), Color.White);
 
             //Draw Button
-            int i = 0;
-            foreach(RotRectangle rotRectangle in ButtonHitboxes)
+            int j = 0;
+            foreach (Vector2 vector2 in buttons)
             {
-                //spriteBatch.DrawString(spriteFont, "Play", rotRectangle.Center, Color.Black);
-                switch (i)
+                switch (j)
                 {
                     case 0:
-                        buttonBoxCorners = rotRectangle.Corners;
-                        spriteBatch.DrawString(spriteFont, "Play", buttonBoxCorners[2], Color.Black);   //rotRectangle.Center, Color.Black);
-                        i++;
+                        spriteBatch.DrawString(spriteFont, "Play", buttons[j], Color.Black);
+                        j++;
                         break;
                     case 1:
-                        buttonBoxCorners = rotRectangle.Corners;
-                        spriteBatch.DrawString(spriteFont, "Controls", buttonBoxCorners[2], Color.Black);
-                        i++;
+                        spriteBatch.DrawString(spriteFont, "Controls", buttons[j], Color.Black);
+                        j++;
                         break;
                     case 2:
-                        buttonBoxCorners = rotRectangle.Corners;
-                        spriteBatch.DrawString(spriteFont, "Credits", buttonBoxCorners[2], Color.Black);
-                        i++;
+                        spriteBatch.DrawString(spriteFont, "Options", buttons[j], Color.Black);
+                        j++;
                         break;
                     case 3:
-                        buttonBoxCorners = rotRectangle.Corners;
-                        spriteBatch.DrawString(spriteFont, "Exit", buttonBoxCorners[2], Color.Black);
-                        i++;
+                        spriteBatch.DrawString(spriteFont, "Credits", buttons[j], Color.Black);
+                        j++;
                         break;
-                }   
-                
+                    case 4:
+                        spriteBatch.DrawString(spriteFont, "Exit", buttons[j], Color.Black);
+                        j++;
+                        break;
+
+                }
+
             }
-            
+
             spriteBatch.End();
         }
      
@@ -113,33 +100,37 @@ namespace CarGo
             //TODO: Gamepad Input
             //Check which Button is selected 
             this.Input();
-            if (Keyboard.GetState().IsKeyDown(Keys.Enter) || GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.A))
+
+
+        }
+
+        private void ConfirmSelection()
+        {
+            if (stage == 0)
             {
-                //Play
-                if (CollisionCheck.CheckCollision(carrierBox, ButtonHitboxes[0]) == true)
-                {
-                    soundHorn.Play();
-                    theGame.GameState = GameState.MenuModificationSelection;
-                }
-                //Options (Non existent atm)
-                if (CollisionCheck.CheckCollision(carrierBox, ButtonHitboxes[1]) == true)
-                {
-                    theGame.GameState = GameState.MenuControls;
-                }
+                soundHorn.Play();
+                theGame.GameState = GameState.MenuModificationSelection;
+            }
 
-                //Credits (non existent atm)
-                if (CollisionCheck.CheckCollision(carrierBox, ButtonHitboxes[2]) == true)
-                {
-                    theGame.GameState = GameState.CreditScreen;
-                }
+            if (stage == 1)
+            {
+                theGame.GameState = GameState.MenuControls;
+            }
 
-                //Exit
-                if (CollisionCheck.CheckCollision(carrierBox, ButtonHitboxes[3]) == true)
-                {
-                    soundHorn.Play();
-                    theGame.GameState = GameState.Exit;
-                }
-                
+            if (stage == 2)
+            {
+                theGame.GameState = GameState.CreditScreen;
+            }
+
+            if (stage == 3)
+            {
+                theGame.GameState = GameState.CreditScreen;
+            }
+
+            if (stage == 4)
+            {
+                soundHorn.Play();
+                theGame.GameState = GameState.Exit;
             }
         }
 
@@ -149,75 +140,28 @@ namespace CarGo
             {
                 GamePadState state = GamePad.GetState(PlayerIndex.One);
 
-                if (state.ThumbSticks.Left.Y < 0f && previousState.ThumbSticks.Left.Y == 0)
+                if (state.ThumbSticks.Left.Y < 0f && previousState.ThumbSticks.Left.Y == 0 && stage < 4)
                 {
-                    if (carrierBox.Center.Y >= ButtonHitboxes[3].Center.Y)
-                    { return; }
-                    else
-                    {
-                        carrierBox.Move(new Vector2(0, 100));
-                    }
+
+                    stage++;
+
                 }
-                if (state.ThumbSticks.Left.Y > 0f && previousState.ThumbSticks.Left.Y == 0)
+                if (state.ThumbSticks.Left.Y > 0f && previousState.ThumbSticks.Left.Y == 0 && stage > 0)
                 {
-                    if (carrierBox.Center.Y <= ButtonHitboxes[0].Center.Y)
-                    { return; }
-                    else
-                    {
-                        carrierBox.Move(new Vector2(0, -100));
-                    }
+
+                    stage--;
+
+                }
+
+                if(state.IsButtonUp(Buttons.A)&& previousState.IsButtonDown(Buttons.A))
+                {
+                    ConfirmSelection();
                 }
 
                 previousState = state;
-            }
-            else //use Keyboard
-            {
-                KeyboardState keyboardstate = Keyboard.GetState();
-                //Up
-                if (keyboardstate.IsKeyDown(Keys.W) && previousKeyBoardState.IsKeyUp(Keys.W) || keyboardstate.IsKeyDown(Keys.Up) && previousKeyBoardState.IsKeyUp(Keys.Up))
-                {
-                    if (carrierBox.Center.Y <= ButtonHitboxes[0].Center.Y)
-                    { return; }
-                    else
-                    {
-                        carrierBox.Move(new Vector2(0, -100));
-                    }
-                }
-
-                //Down
-                if (keyboardstate.IsKeyDown(Keys.S) && previousKeyBoardState.IsKeyUp(Keys.S) || keyboardstate.IsKeyDown(Keys.Down) && previousKeyBoardState.IsKeyUp(Keys.Down))
-                {
-                    if (carrierBox.Center.Y >= ButtonHitboxes[3].Center.Y)
-                    { return; }
-                    else
-                    {
-                        carrierBox.Move(new Vector2(0, 100));
-                    }
-                }
-                previousKeyBoardState = keyboardstate;
             }
         }
 
     }
 }
 
-//public void DrawString(SpriteFont spriteFont, string text, Vector2 position, Color color);
-//spriteBatch.Draw(texture, hitbox.Center - offset, null, Color.White, hitbox.RotationRad, hitbox.Offset, 1.0f, SpriteEffects.None, 0f);
-//public void Draw(Texture2D texture, Vector2 position, Rectangle? sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth);
-//public void Draw(Texture2D texture, Vector2 position, Color color);
-//new Vector2(400 + (int)index * 100, 400) spawn car
-
-/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-
-
-namespace CarGo
-{
-*/
